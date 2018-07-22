37af37ffcf1a2fb1eeb8506ae92b992e4fce4208
Ram Komma <ram.komma@cerner.com>
2018-07-22 00:19:43 -0500

    Fix master

--- public/js/main.js
+++ public/js/main.js
@@ -4,37 +4,36 @@ import retrieveMentionedComments from './lib/secretSauce.js';
 
 import {
     getLatLng,
-    getRestrauntFromGoogle,
     getRestrauntsFromGoogle
 } from './lib/google.js';
 import {
     setLoading
 } from './lib/loadingIndicator.js';
 
+const zipCode = 66210;
+const dish = 'biriyani';
+const radius = 25000;
+
 $(document).ready(() => {
     $('button[type=submit]').click(() => setLoading(true));
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
     var latitude, longitude;
-    getLatLng(66210)
-    .then((response) => {
-            latitude = laresponse.json.results[0].geometry.location.lat;
-            longitude = laresponse.json.results[0].geometry.location.lng;
-            return response.json.results[0].geometry.location;})
-    .then((latlng) => {
-<<<<<<< Updated upstream
-    getFourSquareReviews().then(() => {
-        var rest = [];
-        rest = retrieveMentionedComments(fourSquareRestaurantList, "pasta");
-        console.log(rest);
-        console.log("Retrieved Restaurants Details from FourSquare");
-    }).catch((error) => {
-        console.error(error);
-=======
-    getRestaurantsFromLatLong();
->>>>>>> Stashed changes
-    });
-}
+    getLatLng(zipCode)
+        .then((response) => {
+            latitude = response.json.results[0].geometry.location.lat;
+            longitude = response.json.results[0].geometry.location.lng;
+            return response.json.results[0].geometry.location;
+        })
+        .then((latlng) => {
+            getFourSquareReviews(latitude, longitude, radius);
+            getRestrauntsFromGoogle(dish, latitude, longitude, radius, zipCode)
+        })
+        .then((response) => {
+            var rest = [];
+            rest = retrieveMentionedComments(fourSquareRestaurantList, dish);
+            console.log(rest);
+            console.log("Retrieved Restaurants Details from FourSquare");
+        }).catch((error) => {
+            console.error(error);
+        });
+});
