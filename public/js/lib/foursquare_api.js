913f817a55996fd8feec51bb53e11611f0236a9e
Teja <Tejaswi.Gade@Cerner.com>
2018-07-21 23:03:12 -0500

    Promises

--- public/js/lib/foursquare_api.js
+++ public/js/lib/foursquare_api.js
@@ -20,41 +20,77 @@
     }
     today = yyyy + mm + dd;
 
-    function getRestaurantsFromLatLong(latitude = '39.09973', longitude = '-94.57857', radius = 25000) {
+    function getFourSquareReviews(latitude = '39.09973', longitude = '-94.57857', radius = 25000) {
         console.log("Retrieving Restaurants Details from FourSquare");
-        return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
+        //return new Promise((resolve, reject) => {
+        //const xhr = new XMLHttpRequest();
 
-            var comma = ','
-            var ll = latitude.concat(comma, longitude);
-            xhr.onload = event => {
-                if (xhr.status !== 200) {
-                    reject(xhr.status);
-                }
-                const response = JSON.parse(xhr.responseText);
-                resolve(response);
-                //console.log(response.response.venues);
-                //console.log(response.response.venues.id);
-                for (var i = 0; i < response.response.venues.length; i++) {
-                    var zipCode = response.response.venues[i].location.postalCode;
-                    var restaurantWTFId = restaurantKey(response.response.venues[i].name, zipCode);
-                    var restaurant = new Restaurant(restaurantWTFId, "FOURSQUARE", response.response.venues[i].id, response.response.venues[i].name, response.response.venues[i].location.formattedAddress);
-                    restaurantsList.push(restaurant);
-                    getRestaurantsDetails(restaurant);
-                    getRestaurantMenu(restaurant);
-                    console.log(restaurant);
-                }
-                console.log("Retrieved Restaurants Details from FourSquare");
-                //getRestaurantsDetails();
-            };
-            var URL = "https://api.foursquare.com/v2/venues/search?" + "ll=" + ll +
-                "&client_id=" + foursquare_client_id + "&client_secret=" + foursquare_client_secret +
-                "&categoryId=" + category_id +
-                "&radius" + radius + "&v=" + today;
+        var comma = ','
+        var ll = latitude.concat(comma, longitude);
+        //xhr.onload = event => {
+        //    if (xhr.status !== 200) {
+        //        reject(xhr.status);
+        //    }
+        //    const response = JSON.parse(xhr.responseText);
 
-            xhr.open("GET", URL, true);
-            xhr.send();
+        //console.log(response.response.venues);
+        //console.log(response.response.venues.id);
+        // var promises = [];
+        // for (var i = 0; i < response.response.venues.length; i++) {
+        //     var zipCode = response.response.venues[i].location.postalCode;
+        //     var restaurantWTFId = restaurantKey(response.response.venues[i].name, zipCode);
+        //     var restaurant = new Restaurant(restaurantWTFId, "FOURSQUARE", response.response.venues[i].id, response.response.venues[i].name, response.response.venues[i].location.formattedAddress);
+        //     restaurantsList.push(restaurant);
+        //     promises.push(getRestaurantsDetails(restaurant));
+        //     //promises.push(getRestaurantMenu(restaurant));
+        //     //resolve(response);
+        //     // getRestaurantsDetails(restaurant).then(() => {
+        //     //     var rest = [];
+        //     //     rest = retrieveMentionedComments(restaurantsList, "pasta");
+        //     //     console.log(rest);
+        //     //     console.log("Retrieved Restaurants Details from FourSquare");
+        //     // });
+
+        //     //console.log(restaurant);
+        // }
+        // console.log("Retrieved Restaurants Details from FourSquare");
+        // return Promise.all(promises);
+
+        //getRestaurantsDetails();
+        //};
+        var URL = "https://api.foursquare.com/v2/venues/search?" + "ll=" + ll +
+            "&client_id=" + foursquare_client_id + "&client_secret=" + foursquare_client_secret +
+            "&categoryId=" + category_id +
+            "&radius" + radius + "&v=" + today;
+        return fetch(URL).then((response) => {
+            return response.json();
+        }).then((response) => {
+            var promises = [];
+            for (var i = 0; i < response.response.venues.length; i++) {
+                var zipCode = response.response.venues[i].location.postalCode;
+                var restaurantWTFId = restaurantKey(response.response.venues[i].name, zipCode);
+                var restaurant = new Restaurant(restaurantWTFId, "FOURSQUARE", response.response.venues[i].id, response.response.venues[i].name, response.response.venues[i].location.formattedAddress);
+                restaurantsList.push(restaurant);
+                promises.push(getRestaurantsDetails(restaurant));
+                //promises.push(getRestaurantMenu(restaurant));
+                //resolve(response);
+                // getRestaurantsDetails(restaurant).then(() => {
+                //     var rest = [];
+                //     rest = retrieveMentionedComments(restaurantsList, "pasta");
+                //     console.log(rest);
+                //     console.log("Retrieved Restaurants Details from FourSquare");
+                // });
+
+                //console.log(restaurant);
+            }
+            console.log("Retrieved Restaurants Details from FourSquare");
+            return Promise.all(promises);
+            //return Promise.resolve();
         });
+
+        //xhr.open("GET", URL, true);
+        //xhr.send();
+        //});
     }
 
     function getRestaurantsDetails(restaurant) {
@@ -62,12 +98,47 @@
         return new Promise((resolve, reject) => {
             const xhr = new XMLHttpRequest();
 
-            xhr.onload = event => {
-                if (xhr.status !== 200) {
-                    reject(xhr.status);
-                }
-                const response = JSON.parse(xhr.responseText);
-                resolve(response);
+            //xhr.onload = event => {
+            //    if (xhr.status !== 200) {
+            //        reject(xhr.status);
+            //    }
+            // const response = JSON.parse(xhr.responseText);
+
+            // //console.log(response);
+            // var commentsList = [];
+
+            // restaurant.rating = response.response.venue.rating;
+            // //console.log("Restaurant Rating: " + restaurant.rating);
+
+            // restaurant.likes = response.response.venue.likes.count;
+            // //console.log("Restaurant Likes: " + restaurant.restaurantLikes);
+
+            // restaurant.visitsCount = response.response.venue.stats.visitsCount;
+            // //console.log("Restaurant Visits Count: " + restaurant.restaurantVisitsCount);
+
+            // //console.log(response.response.venues);
+            // //console.log(response.response.venues.id);
+            // for (var j = 0; j < response.response.venue.tips.groups.length; j++) {
+            //     for (var k = 0; k < response.response.venue.tips.groups[j].items.length; k++) {
+            //         commentsList.push(response.response.venue.tips.groups[j].items[k].text);
+            //     }
+            // }
+            // restaurant.comments = commentsList;
+            //resolve(response);
+            //console.log(restaurant.commentsList);
+            //console.log("Retrieved Restaurant details from FourSquare for restaurant: " + restaurant.sourceIdentifiers["FOURSQUARE"]);
+            //};
+            var URL = "https://api.foursquare.com/v2/venues/" + restaurant.sourceIdentifiers["FOURSQUARE"] + "?" +
+                "&client_id=" + foursquare_client_id + "&client_secret=" + foursquare_client_secret + "&v=" + today;
+            //console.log(URL);
+            //xhr.open("GET", URL, true);
+            //xhr.send();
+            return fetch(URL).then((response) => {
+                return response.json();
+            }).then((response) => {
+                //const response = JSON.parse(responseText);
+                //const response = responseText.json();
+
                 //console.log(response);
                 var commentsList = [];
 
@@ -88,14 +159,8 @@
                     }
                 }
                 restaurant.comments = commentsList;
-                //console.log(restaurant.commentsList);
-                //console.log("Retrieved Restaurant details from FourSquare for restaurant: " + restaurant.sourceIdentifiers["FOURSQUARE"]);
-            };
-            var URL = "https://api.foursquare.com/v2/venues/" + restaurant.sourceIdentifiers["FOURSQUARE"] + "?" +
-                "&client_id=" + foursquare_client_id + "&client_secret=" + foursquare_client_secret + "&v=" + today;
-            //console.log(URL);
-            xhr.open("GET", URL, true);
-            xhr.send();
+                return Promise.resolve();
+            });
         });
     }
 
@@ -109,7 +174,7 @@
                     reject(xhr.status);
                 }
                 const response = JSON.parse(xhr.responseText);
-                resolve(response);
+
                 //console.log(response);
                 var menuItems = [];
                 var hasMenu = false;
@@ -127,6 +192,7 @@
                     restaurant.hasMenu = true;
                 }
                 restaurant.menu = menuItems;
+                resolve(response);
                 //console.log(restaurant.restaurantMenu);
                 //console.log("Retrieved Restaurant Menu from FourSquare for restaurant: " + restaurant.sourceIdentifiers["FOURSQUARE"]);
             };
@@ -138,4 +204,5 @@
         });
     }
 
-    export default getRestaurantsFromLatLong
+    export default getFourSquareReviews
+    export { restaurantsList }
--- public/js/secretSauce.js
+++ public/js/secretSauce.js
@@ -1,73 +1,76 @@
-
 //$(document).ready(() => {
-  function retrieveMentionedComments(restaurantsList, searchString) {
-    document.write("test12"); //need to commit
-    var refinedSingleWordStrings = searchString(searchString);
+function retrieveMentionedComments(restaurantsList, inputSearchString) {
+
+    //    document.write("test12"); //need to commit
+    //console.log(restaurantsList);
+    var refinedSingleWordStrings = searchString(inputSearchString);
     var searchWordSize = refinedSingleWordStrings.length;
     var found = false;
     //retrieveMentionedComments.mergeResults = [];
 
     //restaurantsList.forEach(oneRestaurant => {
     for (var k = 0; k < restaurantsList.length; k++) {
-      //var commentsSize = restaurantsList[k].comments.length;
-      var commentsToAdd = [];
-      for (var i = 0; i < commentsSize; i++) {
-        for (var j = 0; j < searchWordSize; j++) {
-          found = false;
-          if (restaurantsList[k].comments[i].includes(restaurantsList[k].comments[i]))
-            found = true;
-          else
-            break;
+        console.log(restaurantsList[k]);
+        //var restaurant = 
+        console.log(restaurantsList[k].comments);
+        var commentsSize = (restaurantsList[k].comments || []).length;
+        var commentsToAdd = [];
+        for (var i = 0; i < commentsSize; i++) {
+            for (var j = 0; j < searchWordSize; j++) {
+                found = false;
+                if (restaurantsList[k].comments[i].includes(restaurantsList[k].comments[i]))
+                    found = true;
+                else
+                    break;
+            }
+
+            if (found == true && j == searchWordSize) {
+                commentsToAdd.push(k);
+            }
         }
 
-        if (found == true && j == searchWordSize) {
-          commentsToAdd.push(k);
-        }
-      }
+        //Restaurant to add
+        if (typeof retrieveMentionedComments.mergeResults == 'undefined') {
+            retrieveMentionedComments.mergeResults = new HashMap();
+        } else {
+            //if the restaurant is not already present in the map
+            if (!retrieveMentionedComments.mergeResults.has(restaurantsList[k].wtfId)) {
+                var obj = {
+                    wtfId: restaurantsList[k].wtfId,
+                    name: restaurantsList[k].name,
+                    address: restaurantsList[k].address,
+                    wtfRating: restaurantsList[k].wtfRating,
+                    likes: restaurantsList[k].likes,
+                    visitsCount: restaurantsList[k].visitsCount
 
-      //Restaurant to add
-      if (typeof retrieveMentionedComments.mergeResults == 'undefined') {
-        retrieveMentionedComments.mergeResults = new HashMap();
-      }
-      else {
-        //if the restaurant is not already present in the map
-        if (!retrieveMentionedComments.mergeResults.has(restaurantsList[k].wtfId)) {
-          var obj = {
-            wtfId: restaurantsList[k].wtfId,
-            name: restaurantsList[k].name,
-            address: restaurantsList[k].address,
-            wtfRating: restaurantsList[k].wtfRating,
-            likes: restaurantsList[k].likes,
-            visitsCount: restaurantsList[k].visitsCount
+                    //also need to add comments, menu price etc
+                }
+            } else {
 
-            //also need to add comments, menu price etc
-          }
+            }
         }
-        else {
-
-        }
-      }
 
     }
+    return retrieveMentionedComments.mergeResults;
 
+}
 
-  }
-
-  function searchString(rawString) {
+function searchString(rawString) {
     //rawString = "basdlkf ja232r423!01234889&9"; //need to commit
     var singleWordStrings = rawString.split(" ");
     var refinedSingleWordStrings = []
     singleWordStrings.forEach(element => {
-      refinedSingleWordStrings.push(element.replace(/[^a-zA-Z0-9 ]/g, ""));
-      //document.write("<br>each word:" + element.replace(/[^a-zA-Z0-9 ]/g, ""));
+        refinedSingleWordStrings.push(element.replace(/[^a-zA-Z0-9 ]/g, ""));
+        //document.write("<br>each word:" + element.replace(/[^a-zA-Z0-9 ]/g, ""));
     });
 
     return refinedSingleWordStrings;
 
-  }
-  //retrieveMentionedComments();
+}
+//retrieveMentionedComments();
 //});
 
 
 //clubing the two outputs
 
+export default retrieveMentionedComments
